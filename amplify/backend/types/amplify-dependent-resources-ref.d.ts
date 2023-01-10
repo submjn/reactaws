f518cda0a5380c1e3ad7fcce84364b1fef185ffe
Subash Maharjan <bodhisystems@gmail.com>
2023-01-10 16:11:19 -0500

    pulled from aws

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,7 +5,6 @@ export type AmplifyDependentResourcesAttributes = {
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
